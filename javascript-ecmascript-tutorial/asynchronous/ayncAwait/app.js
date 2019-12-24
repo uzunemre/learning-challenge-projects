8dc0cae99f1962f8c8bf7b15ddd6026ac68868d1
uzunemre <1105012030@ybu.edu.tr>
2019-12-24 16:01:13 +0300

    javascript-ecmascript,await

--- javascript-ecmascript-tutorial/asynchronous/ayncAwait/app.js
+++ javascript-ecmascript-tutorial/asynchronous/ayncAwait/app.js
@@ -8,12 +8,12 @@ test1("test1").then(response => console.log(response));
 async function test2(data) {
     let promise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve("Bu bir değerdir")
+            resolve(data)
         }, 5000)
     });
     let response = await promise; // 5 saniye bekleyecek, await sadece aync fonksiyonlarda çalışır
     console.log(response);
-    console.log("deneme");
+    console.log("test2'den sonra çalışır");
 }
 
 test2("test2");
