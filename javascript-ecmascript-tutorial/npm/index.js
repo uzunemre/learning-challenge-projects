965e12bc5e8400fa1e8fc37713987fb79098e559
uzunemre <emreuzun92@gmail.com>
2019-12-25 23:56:46 +0300

    javascript-ecmascript npm

---
+++ javascript-ecmascript-tutorial/npm/index.js
@@ -0,0 +1,4 @@
+console.log("İndex js çalıştı");
+// npm run start yazılınca index js çalışır. Çünkü package.jsonda start komutu ile index js çalışacağı belirtilir
+// npm install simple-is node modules a simple-is eklenir.(proje klasörüne node_modules altına indirir.global olarak indirilmek istenirse -g parametresi eklenir)
+// npm install yaparak.(pacakage.jsondaki modülleri indirebiliriz)
