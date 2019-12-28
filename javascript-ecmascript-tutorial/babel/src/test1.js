b1082c64ca934e10747b5035163023a4dfe6026d
uzunemre <emreuzun92@gmail.com>
2019-12-29 00:58:58 +0300

    javascript-ecmascript babel,webpack

---
+++ javascript-ecmascript-tutorial/babel/src/test1.js
@@ -0,0 +1,10 @@
+class Person {
+    static test() {
+        console.log("Test");
+    }
+}
+
+
+// .\node_modules\.bin\babel src -d lib bu komut iile kodlarımızı ecmascript 5 e çevirebiliriz.
+// yukarıdaki satır package jsona yazılınca npm run build yazıldığında ecmascript 5 e çevirilir
+
